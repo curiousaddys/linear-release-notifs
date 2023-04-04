53aa6ff1c7c999cba397c995deace9e4f875a604
rayzr522 <rayzr522@gmail.com>
2023-04-03 21:03:41 -0400

    parse commits from payload

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
+import { z } from "zod";
+import { CommitSchema } from "./types";
 
 async function main() {
   if (github.context.eventName !== "push") {
     core.setFailed("This action can only be used on push events.");
     return;
   }
-  console.log("commits:", github.context.payload.commits);
+  const commitsParsed = z
+    .array(CommitSchema)
+    .safeParse(github.context.payload.commits);
+  if (!commitsParsed.success) {
+    core.setFailed(commitsParsed.error);
+    return;
+  }
+  const commits = commitsParsed.data;
+  console.log(
+    commits.map((c) => `[${c.id}] ${c.author.username} - ${c.message}`)
+  );
 }
 
 main().catch((e) => core.setFailed(e));
---
+++ src/types.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+export const AuthorSchema = z.object({
+  email: z.string(),
+  name: z.string(),
+  username: z.string(),
+});
+export type AuthorSchema = z.infer<typeof AuthorSchema>;
+
+export const CommitSchema = z.object({
+  author: AuthorSchema,
+  committer: AuthorSchema,
+  distinct: z.boolean(),
+  id: z.string(),
+  message: z.string(),
+  timestamp: z.date(),
+  tree_id: z.string(),
+  url: z.string().url(),
+});
+export type CommitSchema = z.infer<typeof CommitSchema>;
