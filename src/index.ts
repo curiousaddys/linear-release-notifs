27ce21b3ca365a98fdc536a24db107c2ea6d5789
rayzr522 <rayzr522@gmail.com>
2023-04-03 20:57:29 -0400

    fix eventname check

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as core from "@actions/core";
 import * as github from "@actions/github";
 
 async function main() {
-  if (github.context.action !== "push") {
+  if (github.context.eventName !== "push") {
     core.setFailed("This action can only be used on push events.");
     return;
   }
